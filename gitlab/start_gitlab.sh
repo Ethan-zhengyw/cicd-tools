ce641e739eaa93cfc7dcbc450a80c48ba51fd4ba
root <940922457@qq.com>
2018-09-22 17:08:11 +0800

    add gitlab

---
+++ gitlab/start_gitlab.sh
@@ -0,0 +1,1 @@
+docker run --detach     --hostname gitlab.example.com     --publish 443:443 --publish 80:80 --publish 22:22     --name gitlab  --restart always    --volume /Users/zhengyiwang/gitlab/config:/etc/gitlab     --volume /Users/zhengyiwang/gitlab/logs:/var/log/gitlab     --volume /Users/zhengyiwang/gitlab/data:/var/opt/gitlab     gitlab/gitlab-ce:latest
